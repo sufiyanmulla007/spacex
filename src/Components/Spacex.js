3bc74ba55c8d4c11e55168d349923e47f24ff63c
sufyanmulla007 <mohammedsufiyan79@gmail.com>
2023-01-20 16:13:22 +0530

    task

--- src/Components/Spacex.js
+++ src/Components/Spacex.js
@@ -12,21 +12,62 @@ import TableRow from '@mui/material/TableRow';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import axios from 'axios';
-import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+//model
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import View from './View';
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 export default function Spacex() {
 //   const baseURL= "https://api.spacexdata.com/v3/capsules";
   const [page, setPage] = useState(0);
   const [rows, setRows] = useState([]);
+  const [originalData,setOriginalData]=useState([])
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [isLoading,setIsLoading]=useState(false)
+  //mo
+  const [open, setOpen] = useState(false);
+  // const Navigate=useNavigate();
+  const [data,setData]=useState("");
+  const handleOpen = (item) =>{
+  
+    setOpen(true)
+    setData(item)
+    // Navigate("/view",{state:item})
 
+  };
+  // const customstyle={
+  //   // with:"500px"
+  //   position:"absolute",
+  //   top:"300px",
+  //   left:"200px",
+  //   backGround:""
+  // }
+  const handleClose = () => setOpen(false);
+  
   useEffect(()=>{
+    setIsLoading(true)
     axios.get("https://api.spacexdata.com/v3/capsules")
     .then((res)=>{
     //   console.log(res.data);
     setRows(res.data);
+    setOriginalData(res.data)
+    setIsLoading(false)
     });
         
   },[]);
@@ -39,14 +80,42 @@ export default function Spacex() {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-
+  // const location=useLocation();
+  // console.log("use loaction",  location);
   return (
     <>
+    <div className='sort' style={{display:"flex",padding:"5px"}}>
+      
+    <form action="#">
+          <label htmlFor="sort"></label>
+          <select
+            name="sort"
+            id="sort"
+            onChange={(e)=>{
+              if(e.target.value!==""){
+              const newRows=originalData.filter((el)=>el.status===e.target.value)
+              setRows(newRows)
+              }
+              else setRows(originalData)
+              // console.log(newRows)
+            }}
+            >
+            <option value="">All launches</option>
+            <option value="#" disabled></option>
+            <option value="retired">retired</option>
+            <option value="#" disabled></option>
+            <option value="unknown">unknown</option>
+            <option value="#" disabled></option>
+            <option value="active">active</option> 
+            <option value="destroyed">destroyed</option> 
+          </select>
+        </form>
+        </div>
     <Card sx={{ minWidth: 900, p: 4 }}>
     <CardContent>
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
       <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
+        {!isLoading?<Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
               <TableCell>Sr.No</TableCell>
@@ -81,16 +150,50 @@ export default function Spacex() {
                     <TableCell  align="center">
                      {row.status} 
                     </TableCell>
-                    <Link to="/view">
+                    
                     <TableCell  align="center">
-                    <Button>veiw</Button>
-                    </TableCell>
-                    </Link>
+                    {/* <Button>veiw</Button> */}
+                    <div>
+      <Button onClick={()=>handleOpen(row) }>View</Button>
+      <Modal
+     
+        open={open}
+        
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        // style={{width:"500px"}}
+      >
+          <Box sx={style} >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+              Spacex
+             </Typography>
+                  <View data={data}/>
+                    <Button onClick={handleClose}>Close</Button>
+                     </Box>
+                    
+                      </Modal>
+                     </div>
+                   </TableCell>
+                  
                   </TableRow>
                 );
               })}
           </TableBody>
-        </Table>
+        </Table>: <Stack spacing={1}>
+      {/* For variant="text", adjust the height via font-size */}
+      <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+      {/* For other variants, adjust the size with `width` and `height` */}
+      <Skeleton variant="circular" width={40} height={40} />
+      <Stack spacing={1}>
+      {/* For variant="text", adjust the height via font-size */}
+      <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+      {/* For other variants, adjust the size with `width` and `height` */}
+      <Skeleton variant="circular" width={40} height={40} />
+      <Skeleton variant="rectangular" width={210} height={60} />
+      <Skeleton variant="rounded" width={210} height={60} />
+    </Stack>
+    </Stack>}
       </TableContainer>
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
--- src/Components/View.js
+++ src/Components/View.js
@@ -1,10 +1,34 @@
 import React from 'react'
-
-const View = () => {
+import Table from 'react-bootstrap/Table';
+const View = ({data}) => {
   return (
-    <div>
-      View pange
-    </div>
+    <>
+  {/* <h1> {data.missions[0].name}</h1> 
+  <h2>{data.landings}</h2> */}
+  <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>flight</th>
+          <th>landings</th>
+          <th>type</th>
+          <th>details</th>
+          <th>reuse_count</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{data.missions[0].name}</td>
+          <td>{data.missions[0].flight}</td>
+          <td>{data.landings}</td>
+          <td>{data.type}</td>
+          <td>{data.details}</td>
+          <td>{data.reuse_count}</td>
+        </tr>
+        
+      </tbody>
+    </Table>
+    </>
   );
 };
 
